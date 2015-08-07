1af09f9d1211c6670bbf394adf8c3e35bb189077
Christoph Sterz <christoph.sterz@student.hpi.uni-potsdam.de>
2015-08-07 16:40:37 +0200

    some code

---
+++ evilhaxxor90001/badcode.py
@@ -0,0 +1,35 @@
+'''CREATED BY evilhaxxor9001'''
+import math #not needed
+
+#!!!!! Fix this
+def frxx(x, f =2):
+ y.release()	
+ cv2.destroyAllWindows()
+
+
+
+
+import numpy as fr
+
+import cv2
+#odd
+thiscode = cv2.COLOR_BGR2HSV
+ba_tr_ = cv2.inRange
+#rename somehow.
+x3 = cv2.cvtColor
+x1 = fr.array([18,0xFF, 0b11111111])
+
+y = cv2.VideoCapture(000000000-1+1)
+
+x2 = fr.array([7, 0x0,0])
+
+# shit
+while(3>1):
+        _,f = y.read();	cc = x3(f, code=thiscode);ff = ba_tr_(cc, x2, x1)
+
+        cv2.imshow("window", ff)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+
+#I don't know what this does but it is important!!!!
+frxx(3)
